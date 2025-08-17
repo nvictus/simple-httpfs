2c2e4fb59cba419cb4067c4d8ae58e5dcd15cad5
Nezar Abdennur <nabdennur@gmail.com>
2025-08-17 13:39:36 -0400

    refactor: Change file size inference for http, and set default s3 to unsigned

--- simple_httpfs/httpfs.py
+++ simple_httpfs/httpfs.py
@@ -1,22 +1,18 @@
 import collections
 import logging
 import os
-import os.path as op
 import re
-import sys
-import traceback
 from errno import EIO, ENOENT
 from ftplib import FTP
 from stat import S_IFDIR, S_IFREG
-from threading import Timer
 from time import sleep, time
 from urllib.parse import urlparse
 
 import boto3
+from botocore import UNSIGNED
 import diskcache as dc
-import numpy as np
 import requests
-from fuse import FUSE, FuseOSError, LoggingMixIn, Operations
+from fuse import FuseOSError, LoggingMixIn, Operations
 from tenacity import (
     retry,
     stop_after_attempt,
@@ -24,18 +20,11 @@ from tenacity import (
     wait_fixed,
     wait_random,
 )
-
 import slugid
 
-CLEANUP_INTERVAL = 60
-CLEANUP_EXPIRED = 60
-
-REPORT_INTERVAL = 60
 
 DISK_CACHE_SIZE_ENV = "HTTPFS_DISK_CACHE_SIZE"
 DISK_CACHE_DIR_ENV = "HTTPFS_DISK_CACHE_DIR"
-
-
 FALSY = {0, "0", False, "false", "False", "FALSE", "off", "OFF"}
 
 
@@ -63,17 +52,21 @@ class LRUCache:
     def __len__(self):
         return len(self.cache)
 
+    def get(self, key, default=None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
 
 class FtpFetcher:
     def server_path(self, url):
         o = urlparse(url)
-
         return (o.netloc, o.path)
 
     def login(self, server):
         ftp = FTP(server)
         ftp.login()
-
         try:
             # do a retrbinary on a non-existent file
             # to set the transfer mode to binary
@@ -81,27 +74,24 @@ class FtpFetcher:
             ftp.retrbinary(slugid.nice(), lambda x: x + 1)
         except:
             pass
-
         return ftp
 
     def get_size(self, url):
         (server, path) = self.server_path(url)
-
         ftp = self.login(server)
         size = ftp.size(path)
         ftp.close()
         return size
 
     def get_data(self, url, start, end):
-        import time
-
         (server, path) = self.server_path(url)
+
         ftp = self.login(server)
-        conn = ftp.transfercmd("RETR {}".format(path), rest=start)
+        conn = ftp.transfercmd(f"RETR {path}", rest=start)
 
         amt = end - start
         chunk_size = 1 << 15
-        data = []
+        data = b""
         while len(data) < amt:
             chunk = conn.recv(chunk_size)
             if chunk:
@@ -109,18 +99,13 @@ class FtpFetcher:
             else:
                 break
         if len(data) < amt:
-            data += [0] * (amt - len(data))
+            data += b'\x00' * (amt - len(data))
         else:
             data = data[:amt]
 
         ftp.close()
-        t2 = time.time()
-        return np.array(data, dtype=np.uint8)
 
-
-def is_403(value):
-    """Return True if the error is a 403 exception"""
-    return value is not None
+        return data
 
 
 class HttpFetcher:
@@ -128,6 +113,7 @@ class HttpFetcher:
 
     def __init__(self, logger):
         self.logger = logger
+        self.timeout = 5
         if not self.SSL_VERIFY:
             logger.warning(
                 "You have set ssl certificates to not be verified. "
@@ -135,35 +121,47 @@ class HttpFetcher:
                 "http://docs.python-requests.org/en/master/user/advanced/#ssl-cert-verification"
             )
 
-    def get_size(self, url):
+    def get_size(self, url: str) -> int:
         try:
-            head = requests.head(url, allow_redirects=True, verify=self.SSL_VERIFY)
-            return int(head.headers["Content-Length"])
-        except:
-            head = requests.get(
+            head = requests.head(
                 url,
                 allow_redirects=True,
                 verify=self.SSL_VERIFY,
-                headers={"Range": "bytes=0-1"},
+                timeout=self.timeout
             )
-            crange = head.headers["Content-Range"]
-            match = re.search(r"/(\d+)$", crange)
-            if match:
-                return int(match.group(1))
+        except requests.exceptions.Timeout:
+            self.logger.info(f"Timeout occurred while fetching head for {url}")
+            raise FuseOSError(EIO)
 
-            self.logger.error(traceback.format_exc())
-            raise FuseOSError(ENOENT)
+        if head.ok:
+            if "Content-Length" in head.headers:
+                return int(head.headers["Content-Length"])
+            else:
+                # Try the byte range request trick
+                r = requests.get(
+                    url,
+                    allow_redirects=True,
+                    verify=self.SSL_VERIFY,
+                    timeout=self.timeout,
+                    headers={"Range": "bytes=0-0"},
+                )
+                content_range = r.headers.get("Content-Range", "")
+                match = re.search(r"/(\d+)$", content_range)
+                if match:
+                    return int(match.group(1))
+
+            self.logger.info(f"Failed to get size for {url}")
+            return 0
+
+        # Request failed: No such file or directory
+        raise FuseOSError(ENOENT)
 
     @retry(wait=wait_fixed(1) + wait_random(0, 2), stop=stop_after_attempt(2))
-    def get_data(self, url, start, end):
-        headers = {"Range": "bytes={}-{}".format(start, end), "Accept-Encoding": ""}
-        self.logger.info("getting %s %s %s", url, start, end)
+    def get_data(self, url: str, start: int, end: int) -> bytes:
+        headers = {"Range": f"bytes={start}-{end}", "Accept-Encoding": ""}
         r = requests.get(url, headers=headers)
-        self.logger.info("got %s", r.status_code)
-
         r.raise_for_status()
-        block_data = np.frombuffer(r.content, dtype=np.uint8)
-        return block_data
+        return r.content
 
 
 class S3Fetcher:
@@ -172,34 +170,32 @@ class S3Fetcher:
     def __init__(self, aws_profile, logger):
         self.logger = logger
         self.logger.info("Creating S3Fetcher with aws_profile=%s", aws_profile)
+        config = None
+        if aws_profile is None:
+            config = boto3.session.Config(signature_version=UNSIGNED)
         self.session = boto3.Session(profile_name=aws_profile)
-        self.client = self.session.client("s3")
-        pass
+        self.client = self.session.client("s3", config=config)
 
     def parse_bucket_key(self, url):
         url_parts = urlparse(url, allow_fragments=False)
         bucket = url_parts.netloc
         key = url_parts.path.strip("/")
-
         return bucket, key
 
     def get_size(self, url):
         bucket, key = self.parse_bucket_key(url)
-
         response = self.client.head_object(Bucket=bucket, Key=key)
         size = response["ContentLength"]
         return size
 
     @retry(wait=wait_exponential(multiplier=1, min=4, max=10))
-    def get_data(self, url, start, end):
+    def get_data(self, url: str, start: int, end: int) -> bytes:
         bucket, key = self.parse_bucket_key(url)
-        obj = boto3.resource("s3").Object(bucket, key)
         stream = self.client.get_object(
             Bucket=bucket, Key=key, Range="bytes={}-{}".format(start, end)
         )["Body"]
         contents = stream.read()
-        block_data = np.frombuffer(contents, dtype=np.uint8)
-        return block_data
+        return contents
 
 
 class HttpFs(LoggingMixIn, Operations):
@@ -218,19 +214,11 @@ class HttpFs(LoggingMixIn, Operations):
         aws_profile=None,
         logger=None,
     ):
-        self.lru_cache = LRUCache(capacity=lru_capacity)
-        self.lru_attrs = LRUCache(capacity=lru_capacity)
-        self.schema = schema
         self.logger = logger
-        self.last_report_time = 0
-        self.total_requests = 0
-        self.getting = set()
-
         if not self.logger:
             self.logger = logging.getLogger(__name__)
 
-        self.logger.info("Starting with disk_cache_size: %d", disk_cache_size)
-
+        self.schema = schema
         if schema == "http" or schema == "https":
             self.fetcher = HttpFetcher(self.logger)
         elif schema == "ftp":
@@ -238,157 +226,91 @@ class HttpFs(LoggingMixIn, Operations):
         elif schema == "s3":
             self.fetcher = S3Fetcher(aws_profile, self.logger)
         else:
-            raise ("Unknown schema: {}".format(schema))
+            raise ValueError(f"Unknown schema: {schema}")
 
+        self.logger.info(f"Starting with disk_cache_size: {disk_cache_size}")
+        self.lru_cache = LRUCache(capacity=lru_capacity)
+        self.lru_attrs = LRUCache(capacity=lru_capacity)
         self.disk_cache = dc.Cache(disk_cache_dir, size_limit=disk_cache_size)
 
+        self.total_requests = 0
         self.total_blocks = 0
         self.lru_hits = 0
         self.lru_misses = 0
-
         self.disk_hits = 0
         self.disk_misses = 0
         self.block_size = block_size
-
-    def getSize(self, url):
-        try:
-            return self.fetcher.get_size(url)
-        except Exception as ex:
-            self.logger.exception(ex)
-            raise
+        self.getting = set()
 
     def getattr(self, path, fh=None):
-        try:
-            if path in self.lru_attrs:
-                return self.lru_attrs[path]
-
-            if path == "/":
-                self.lru_attrs[path] = dict(st_mode=(S_IFDIR | 0o555), st_nlink=2)
-                return self.lru_attrs[path]
-
-            if (
-                path[-2:] != ".."
-                and not path.endswith("..-journal")
-                and not path.endswith("..-wal")
-            ):
-                return dict(st_mode=(S_IFDIR | 0o555), st_nlink=2)
-
-            url = "{}:/{}".format(self.schema, path[:-2])
-
-            # there's an exception for the -jounral files created by SQLite
-            if not path.endswith("..-journal") and not path.endswith("..-wal"):
-                size = self.getSize(url)
-            else:
-                size = 0
-
-            # logging.info("head: {}".format(head.headers))
-            # logging.info("status_code: {}".format(head.status_code))
-            # print("url:", url, "head.url", head.url)
-
-            if size is not None:
-                self.lru_attrs[path] = dict(
-                    st_mode=(S_IFREG | 0o644),
-                    st_nlink=1,
-                    st_size=size,
-                    st_ctime=time(),
-                    st_mtime=time(),
-                    st_atime=time(),
-                )
-            else:
-                self.lru_attrs[path] = dict(st_mode=(S_IFDIR | 0o555), st_nlink=2)
-
+        if path in self.lru_attrs:
             return self.lru_attrs[path]
-        except Exception as ex:
-            self.logger.exception(ex)
-            raise
 
-    def unlink(self, path):
-        return 0
+        if path == "/":
+            self.lru_attrs[path] = dict(st_mode=(S_IFDIR | 0o555), st_nlink=2)
+            return self.lru_attrs[path]
 
-    def create(self, path, mode, fi=None):
-        return 0
+        if path[-2:] != "..":
+            return dict(st_mode=(S_IFDIR | 0o555), st_nlink=2)
 
-    def write(self, path, buf, size, offset, fip):
-        return 0
+        url = f"{self.schema}:/{path[:-2]}"
+        size = self.fetcher.get_size(url)
+        if size is not None:
+            self.lru_attrs[path] = dict(
+                st_mode=(S_IFREG | 0o644),
+                st_nlink=1,
+                st_size=size,
+                st_ctime=time(),
+                st_mtime=time(),
+                st_atime=time(),
+            )
+        else:
+            self.lru_attrs[path] = dict(
+                st_mode=(S_IFDIR | 0o555),
+                st_nlink=2
+            )
+
+        return self.lru_attrs[path]
 
     def read(self, path, size, offset, fh):
-        t1 = time()
+        url = f"{self.schema}:/{path[:-2]}"
+        self.total_requests += 1
+        final_offset = offset + size
 
-        self.logger.debug("read %s %s %s", path, offset, size)
+        self.logger.info(
+            f"path: {path}\n"
+            f"url: {url}\n"
+            f"range: {offset} - {offset + size - 1}\n"
+            f"request_size (KB): {size / 2 ** 10:.2f}\n"
+        )
 
-        if t1 - self.last_report_time > REPORT_INTERVAL:
-            """
-            self.logger.info(
-                "lru hits: {} lru misses: {} disk hits: {} total_requests: {}".format(
-                    self.lru_hits,
-                    self.lru_misses,
-                    self.disk_hits,
-                    self.disk_misses,
-                    self.total_requests,
-                )
-            )
-            """
-            pass
-        try:
-            self.total_requests += 1
+        output = b""
+        curr_offset = offset
 
-            attr = self.getattr(path)
-            url = "{}:/{}".format(self.schema, path[:-2])
+        while curr_offset < final_offset:
+            block_num = curr_offset // self.block_size
+            block_id = (url, block_num)
+            block_start = curr_offset % self.block_size
+            remaining = final_offset - curr_offset
+            data_size = min(self.block_size - block_start, remaining)
 
-            self.logger.debug("read url: {}".format(url))
-            self.logger.debug(
-                "offset: {} - {} request_size (KB): {:.2f} block: {}".format(
-                    offset,
-                    offset + size - 1,
-                    size / 2 ** 10,
-                    offset // self.block_size,
-                )
-            )
-            output = np.zeros((size,), np.uint8)
+            # Fetch the block
+            while block_id in self.getting:
+                sleep(0.05)
+            self.getting.add(block_id)
+            block = self.read_block(url, block_num)
+            self.getting.remove(block_id)
+            if not len(block):
+                self.logger.info("empty block")
+                break
 
-            t1 = time()
+            # Extract only the portion we need from this block
+            output += block[block_start:block_start + data_size]
+            curr_offset += data_size
 
-            # nothing fetched yet
-            last_fetched = -1
-            curr_start = offset
+        return output
 
-            while last_fetched < offset + size:
-                block_num = curr_start // self.block_size
-                block_start = self.block_size * (curr_start // self.block_size)
-
-                block_id = (url, block_num)
-                while block_id in self.getting:
-                    sleep(0.05)
-
-                self.getting.add(block_id)
-                block_data = self.get_block(url, block_num)
-                self.getting.remove(block_id)
-
-                data_start = (
-                    curr_start - (curr_start // self.block_size) * self.block_size
-                )
-
-                data_end = min(self.block_size, offset + size - block_start)
-                data = block_data[data_start:data_end]
-
-                d_start = curr_start - offset
-                output[d_start : d_start + len(data)] = data
-
-                last_fetched = curr_start + (data_end - data_start)
-                curr_start += data_end - data_start
-
-            bts = bytes(output)
-
-            return bts
-
-        except Exception as ex:
-            self.logger.exception(ex)
-            raise
-
-    def destroy(self, path):
-        self.disk_cache.close()
-
-    def get_block(self, url, block_num):
+    def read_block(self, url, block_num):
         """
         Get a data block from a URL. Blocks are 256K bytes in size
 
@@ -399,37 +321,39 @@ class HttpFs(LoggingMixIn, Operations):
         block_num: int
             The # of the 256K'th block of this file
         """
-        cache_key = "{}.{}.{}".format(url, self.block_size, block_num)
-        cache = self.disk_cache
-
+        cache_key = f"{url}.{self.block_size}.{block_num}"
         self.total_blocks += 1
 
-        if cache_key in self.lru_cache:
+        block = self.lru_cache.get(cache_key, None)
+        if block is not None:
             self.lru_hits += 1
-            hit = self.lru_cache[cache_key]
-            return hit
-        else:
-            self.lru_misses += 1
+            return block
+        self.lru_misses += 1
 
-            if cache_key in self.disk_cache:
-                self.logger.info("cache hit: %s", cache_key)
-                try:
-                    block_data = self.disk_cache[cache_key]
-                    self.disk_hits += 1
-                    self.lru_cache[cache_key] = block_data
-                    return block_data
-                except KeyError:
-                    pass
+        block = self.disk_cache.get(cache_key, None)
+        if block is not None:
+            self.disk_hits += 1
+            self.lru_cache[cache_key] = block
+            return block
+        self.disk_misses += 1
 
-            self.disk_misses += 1
-            block_start = block_num * self.block_size
+        self.logger.info(f"Fetching block {cache_key}...")
+        start = block_num * self.block_size
+        block = self.fetcher.get_data(
+            url, start, start + self.block_size - 1
+        )
+        self.lru_cache[cache_key] = block
+        self.disk_cache[cache_key] = block
+        return block
 
-            self.logger.info("getting data %s", cache_key)
-            block_data = self.fetcher.get_data(
-                url, block_start, block_start + self.block_size - 1
-            )
+    def unlink(self, path):
+        return 0
 
-            self.lru_cache[cache_key] = block_data
-            self.disk_cache[cache_key] = block_data
+    def create(self, path, mode, fi=None):
+        return 0
 
-        return block_data
+    def write(self, path, buf, size, offset, fip):
+        return 0
+
+    def destroy(self, path):
+        self.disk_cache.close()
